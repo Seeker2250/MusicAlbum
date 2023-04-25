86e209e94ca321538e2bb38ae78489646a17e60a
Seeker2250 <a01044777016@gmail.com>
2023-04-25 21:18:21 +0900

    commit 1 -- UI Design Complete

---
+++ app/src/main/java/com/example/music2/AudioImage.java
@@ -0,0 +1,16 @@
+package com.example.music2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AudioImage extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //audio image 띄워줘 화면에
+        setContentView(R.layout.audio_image);
+        //git 왜 안돼
+    }
+}

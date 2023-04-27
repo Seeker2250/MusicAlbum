95c545e93d9d21900b85969684537bfe14b8d01f
Seeker2250 <wnsdnals@naver.com>
2023-04-27 11:20:35 +0900

    Music Album Complete

--- app/src/main/java/com/example/music2/AudioImage.java
+++ app/src/main/java/com/example/music2/AudioImage.java
@@ -1,16 +1,59 @@
 package com.example.music2;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AudioImage extends AppCompatActivity {
+    MediaPlayer mp = new MediaPlayer();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //audio image 띄워줘 화면에
         setContentView(R.layout.audio_image);
-        //git 왜 안돼
-    }
+
+        setTitle("노래 재생");
+
+        //tag 추출
+        Intent it = getIntent();
+        String tag = it.getStringExtra("it_tag");
+
+        //object 획득
+        TextView title = findViewById(R.id.title);
+        ImageView song_image = findViewById(R.id.song_image);
+        TextView lyrics = findViewById(R.id.lyrics);
+
+        Resources res = getResources();
+
+        int stringId;
+        String mkKey;
+
+        stringId = res.getIdentifier("title"+tag, "string", getPackageName());
+        mkKey = res.getString(stringId);
+        title.setText(mkKey);//제목 출력
+
+        stringId = res.getIdentifier("song_image"+tag, "string", getPackageName());
+        mkKey = res.getString(stringId);
+        int imageId = res.getIdentifier(mkKey, "drawable", getPackageName());
+        song_image.setImageResource(imageId);//이미지 출력
+
+        stringId = res.getIdentifier("lyrics"+tag, "string", getPackageName());
+        mkKey = res.getString(stringId);
+        lyrics.setText(mkKey);//가사 출력
+
+        stringId = res.getIdentifier("audio"+tag, "string", getPackageName());
+        mkKey = res.getString(stringId);
+        int audioId = res.getIdentifier(mkKey, "raw", getPackageName());//오디오 재생
+
+        mp = MediaPlayer.create(this, audioId);
+        mp.setLooping(false);
+        mp.start();
+    } //end of onCreate
 }
--- app/src/main/java/com/example/music2/MainActivity.java
+++ app/src/main/java/com/example/music2/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.music2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer mp = new MediaPlayer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("노래 목록");//Activity 이름 수정
+    }
+    public void play(View v) {
+        int id = v.getId();
+        LinearLayout layout = (LinearLayout) findViewById(id);//LinearLayout 취득
+        String tag = (String) layout.getTag();
+
+        Intent it = new Intent(this, AudioImage.class);//Intent object 생성
+        it.putExtra("it_tag", tag);
+        startActivity(it);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mp.isPlaying()){
+            mp.stop();
+        }
     }
 }
